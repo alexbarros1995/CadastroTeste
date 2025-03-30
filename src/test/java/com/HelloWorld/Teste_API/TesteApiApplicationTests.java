1e2da13e833a9988c07b310ca2f86b299927200b
Alex Barros <alexoficial66@gmail.com>
2025-03-30 17:07:41 -0300

    primeiro commit

---
+++ src/test/java/com/HelloWorld/Teste_API/TesteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HelloWorld.Teste_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
